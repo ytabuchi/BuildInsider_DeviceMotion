df8a5c0d27a1b02766ef9ace03124016bb78630b
Yoshito Tabuchi on x220 <ytabuchi.xlsoft@gmail.com>
2016-02-26 20:20:17 +0900

    Imprement accelerometer method

--- BI_Accelerometer/BI_Accelerometer/BI_Accelerometer/App.cs
+++ BI_Accelerometer/BI_Accelerometer/BI_Accelerometer/App.cs
@@ -11,20 +11,7 @@ namespace BI_Accelerometer
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                         new Label {
-                             HorizontalTextAlignment = TextAlignment.Center,
-                             Text = "Welcome to Xamarin Forms!"
-                         }
-                     }
-                }
-            };
+            MainPage = new MainPageCS();
         }
 
         protected override void OnStart()
