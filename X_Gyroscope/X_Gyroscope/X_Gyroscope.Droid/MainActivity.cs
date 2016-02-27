64778600a3b1dce4a3e2e97778f7f7ca559c556b
Yoshito Tabuchi on MBA-VS <ytabuchi.xlsoft@gmail.com>
2016-02-27 22:56:15 +0900

    Add Xamarin Native Gyroscope project

---
+++ X_Gyroscope/X_Gyroscope/X_Gyroscope.Droid/MainActivity.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace X_Gyroscope.Droid
+{
+	[Activity (Label = "X_Gyroscope.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity, ISensorEventListener
+    {
+        static readonly object syncLock = new object();
+        SensorManager sensorManager;
+        TextView xLabel;
+        TextView yLabel;
+        TextView zLabel;
+        Button startButton;
+        Button stopButton;
+        ObservableCollection<double> gyroData;
+
+        protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+            sensorManager = (SensorManager)GetSystemService(SensorService);
+
+            xLabel = FindViewById<TextView>(Resource.Id.xLabel);
+            yLabel = FindViewById<TextView>(Resource.Id.yLabel);
+            zLabel = FindViewById<TextView>(Resource.Id.zLabel);
+
+            startButton = FindViewById<Button>(Resource.Id.StartButton);
+            startButton.Click += (sender, e) =>
+            {
+                gyroData = new ObservableCollection<double>();
+                sensorManager.RegisterListener(this, sensorManager.GetDefaultSensor(SensorType.Gyroscope), SensorDelay.Normal);
+            };
+
+            stopButton = FindViewById<Button>(Resource.Id.StopButton);
+            stopButton.Click += (sender, e) =>
+            {
+                sensorManager.UnregisterListener(this);
+            };
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            sensorManager.UnregisterListener(this);
+        }
+
+        public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            lock (syncLock)
+            {
+                gyroData.Add(e.Values[0]);
+                gyroData.Add(e.Values[1]);
+                gyroData.Add(e.Values[2]);
+                var shake = gyroData.Where(gyroData => Math.Abs(gyroData) > 3).Count();
+                if (shake > 3)
+                {
+                    sensorManager.UnregisterListener(this);
+
+                    var dialog = new AlertDialog.Builder(this);
+                    dialog.SetTitle("Gyroscope");
+                    dialog.SetMessage("Shaked!");
+                    dialog.SetNegativeButton("OK", (_, __) => { });
+                    dialog.Show();
+                }
+
+                xLabel.Text = e.Values[0].ToString("0.0000");
+                yLabel.Text = e.Values[1].ToString("0.0000");
+                zLabel.Text = e.Values[2].ToString("0.0000");
+            }
+        }
+    }
+}
+
+
---
+++ X_Gyroscope/X_Gyroscope/X_Gyroscope.iOS/ViewController.cs
@@ -0,0 +1,61 @@
+﻿using CoreMotion;
+using Foundation;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UIKit;
+
+namespace X_Gyroscope.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+        private CMMotionManager motionManager;
+        ObservableCollection<double> gyroData;
+
+        public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+            motionManager = new CMMotionManager();
+
+            StartButton.TouchUpInside += (sender, e) =>
+            {
+                gyroData = new ObservableCollection<double>();
+                motionManager.StartGyroUpdates(NSOperationQueue.CurrentQueue, (data, error) =>
+                {
+                    gyroData.Add(data.RotationRate.x);
+                    gyroData.Add(data.RotationRate.y);
+                    gyroData.Add(data.RotationRate.z);
+                    var shake = gyroData.Where(gyroData => Math.Abs(gyroData) > 3).Count();
+                    if (shake > 3)
+                    {
+                        motionManager.StopGyroUpdates();
+
+                        var alert = new UIAlertView("Gyroscope", "Shaked!", null, "OK");
+                        alert.Show();
+                    }
+
+                    this.xLabel.Text = data.RotationRate.x.ToString("0.0000");
+                    this.yLabel.Text = data.RotationRate.y.ToString("0.0000");
+                    this.zLabel.Text = data.RotationRate.z.ToString("0.0000");
+                });
+            };
+
+            StopButton.TouchUpInside += (sender, e) =>
+            {
+                motionManager.StopGyroUpdates();
+            };
+        }
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
