7696db791904e70cedc9a17f2a591824ee7bc982
Yoshito Tabuchi on MBA-VS <ytabuchi.xlsoft@gmail.com>
2016-02-27 22:04:00 +0900

    Add Xamarin Native Accelerometer project

---
+++ X_Accelerometer/X_Accelerometer/X_Accelerometer.iOS/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using CoreMotion;
+using Foundation;
+using System;
+
+using UIKit;
+
+namespace X_Accelerometer.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+        private CMMotionManager motionManager;
+
+        public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+            motionManager = new CMMotionManager();
+
+            StartButton.TouchUpInside += (sender, e) =>
+            {
+                motionManager.StartAccelerometerUpdates(NSOperationQueue.CurrentQueue, (data, error) =>
+                {
+                    this.xLabel.Text = data.Acceleration.X.ToString("0.0000");
+                    this.yLabel.Text = data.Acceleration.Y.ToString("0.0000");
+                    this.zLabel.Text = data.Acceleration.Z.ToString("0.0000");
+                });
+            };
+
+            StopButton.TouchUpInside += (sender, e) =>
+            {
+                motionManager.StopAccelerometerUpdates();
+            };
+        }
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
