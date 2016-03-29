91056bb0c01adef3c9f5e95be3ba278d6bb865ac
Yoshito Tabuchi on Y510p <ytabuchi.xlsoft@gmail.com>
2016-03-29 16:54:39 +0900

    Add BI_Compass

---
+++ BI_Compass/BI_Compass/BI_Compass/MainPageXaml.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DeviceMotion.Plugin;
+using DeviceMotion.Plugin.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BI_Compass
+{
+    public partial class MainPageXaml : ContentPage
+    {
+        IDeviceMotion motion = CrossDeviceMotion.Current;
+
+        public MainPageXaml()
+        {
+            InitializeComponent();
+        }
+
+        private void StartClicked(object sender, EventArgs e)
+        {
+            motion.Start(MotionSensorType.Compass, MotionSensorDelay.Default);
+            if (motion.IsActive(MotionSensorType.Compass))
+            {
+                motion.SensorValueChanged += (object s, SensorValueChangedEventArgs a) =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        compassLabel.Text = string.Format($"{a.Value.Value:N0}");
+                    });
+                };
+            }
+        }
+
+        private void StopClicked(object sender, EventArgs e)
+        {
+            motion.Stop(MotionSensorType.Compass);
+        }
+    }
+}
